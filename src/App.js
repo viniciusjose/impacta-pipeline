2a4bada33e95e4676a4f0346b4a44172a60bb0e6
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2023-07-27 13:38:39 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={mba} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to IMPACTA TECNOLOGIA - MBA AS_06</h1>
+          <h1 className="App-title">Welcome to Devops UFSCAR</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
